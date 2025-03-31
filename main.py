99106d0c88cd837ec19ceb7bc17a93bf591387bb
Leobrv17 <leo.breviere@gmail.com>
2025-03-31 15:01:38 +0200

    upgrade

--- main.py
+++ main.py
@@ -11,24 +11,30 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-def launch_game(game_name, screen, nb_players):
-    """ Fonction pour lancer un jeu donné en fonction du nom du jeu """
 
-    if nb_players == 1:
-        twoPlayers = False
-    else:
-        twoPlayers = True
+def launch_game(game_name, window, twoPlayer):
+    # Déterminez le chemin du dossier du jeu (par exemple game1 ou game2)
+    game_path = os.path.abspath(f"./games/{game_name}")
+
+    # Ajoutez ce dossier au sys.path pour pouvoir importer les fichiers de ce jeu
+    sys.path.insert(0, game_path)
 
     try:
-        game_module = importlib.import_module(f"games.{game_name}.main")  # Importer le jeu (ex. 'games.game1.main')
+        # Importez les modules nécessaires à partir du jeu
+        # Assurez-vous que les fichiers nécessaires sont bien présents dans le dossier du jeu
+        game_module = importlib.import_module("main")  # Import du main.py de chaque jeu
+
+        # Exécutez la logique du jeu
+        score_p1, score_p2, = game_module.main(window, twoPlayer)  # Remplacez par la fonction principale du jeu
     except ModuleNotFoundError:
         print(f"Erreur : Le module pour {game_name} n'a pas été trouvé.")
-        return 0, 0, 0
+    finally:
+        # Retirer le chemin ajouté à sys.path après utilisation
+        sys.path.pop(0)
+        return score_p1, score_p2, score_p1 + score_p2
 
-    # Lancer la fonction main du jeu en passant le Screen
-    score_p1, score_p2, total_score = game_module.main(screen,twoPlayers)
+    return score_p1, score_p2, score_p1 + score_p2
 
-    return score_p1, score_p2, total_score
 
 if __name__ == "__main__":
     load_dotenv()
@@ -58,11 +64,12 @@ if __name__ == "__main__":
                 screen_running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:  # Si l'utilisateur appuie sur "1"
-                    score_p1, score_p2, total_score = launch_game("game1", screen.screen,1)
+                    score_p1, score_p2, total_score = launch_game("game1", screen.screen, 1)
+                    print(score_p1, score_p2,total_score)
                     screen_running = False  # Quitter le menu après avoir joué
 
                 if event.key == pygame.K_2:  # Si l'utilisateur appuie sur "2"
-                    score_p1, score_p2, total_score = launch_game("game2", screen.screen,2)
+                    score_p1, score_p2, total_score = launch_game("game2", screen.screen, 2)
                     screen_running = False  # Quitter le menu après avoir joué
 
-        time.sleep(0.1)
+        time.sleep(1)
